8e5d6fe65b633951606c9150d5110296c1361a00
l0o0gy <minamahone9620@gmail.com>
2024-02-05 00:12:36 +0300

    homework2

---
+++ index.js
@@ -0,0 +1,9 @@
+var numbera =15;
+var numberb =10;
+
+console.log("a grater than b", numbera>numberb);
+console.log("a less than b", numbera<numberb);
+console.log("less or equals",numbera>=numberb);
+console.log("greater or egual",numbera<=numberb);
+console.log("equal to",numbera>numberb && numberb==3);
+console.log("not equal to",numbera<numberb|| numberb!=12); 
